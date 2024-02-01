b2db4b9471376864d16e8983dc7cbe44170aff29
Sholahuddin Al Ayubi <sholahuddin95@gmail.com>
2024-02-01 10:58:52 +0700

    Add Golang Web App: Hello World

---
+++ web_app/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func handlerIndex(w http.ResponseWriter, r *http.Request) {
+	logRequest(r)
+	var pesan = "Welcome"
+	w.Write([]byte(pesan))
+}
+
+func handlerHello(w http.ResponseWriter, r *http.Request) {
+	logRequest(r)
+	var pesan = "HELLO WORLD"
+	w.Write([]byte(pesan))
+}
+
+func logRequest(r *http.Request) {
+	fmt.Printf("[%s] %s %s\n", time.Now().Format("2006-01-02 15:04:05"), r.Method, r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/index", handlerIndex)
+	http.HandleFunc("/hello", handlerHello)
+
+	var alamat = "localhost:9000"
+	fmt.Printf("Server started at %s\n", alamat)
+	err := http.ListenAndServe(alamat, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
