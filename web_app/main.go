a665e1945f4a00f3c7f3fdea18cd5bec4f8014d4
Sholahuddin Al Ayubi <sholahuddin95@gmail.com>
2024-02-01 11:27:32 +0700

    Fungsi http.HandleFunc() digunakan untuk registrasi rute/endpoint dan handler-nya.

--- web_app/main.go
+++ web_app/main.go
@@ -22,11 +22,18 @@ func logRequest(r *http.Request) {
 	fmt.Printf("[%s] %s %s\n", time.Now().Format("2006-01-02 15:04:05"), r.Method, r.URL.Path)
 }
 
+func logRequestTesting(r *http.Request) {
+	fmt.Printf("[%s] %s %s (from /testing)\n", time.Now().Format("2006-01-02 15:04:05"), r.Method, r.URL.Path)
+}
+
 func main() {
 	http.HandleFunc("/", handlerIndex)
 	http.HandleFunc("/index", handlerIndex)
 	http.HandleFunc("/hello", handlerHello)
-
+	http.HandleFunc("/testing", func(w http.ResponseWriter, r *http.Request) {
+		logRequestTesting(r)
+		w.Write([]byte("Hello Testing"))
+	})
 	var alamat = "localhost:9000"
 	fmt.Printf("Server started at %s\n", alamat)
 	err := http.ListenAndServe(alamat, nil)
