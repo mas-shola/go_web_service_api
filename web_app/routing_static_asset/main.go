6babf544db80dee6b1cafc5b67a3ce64a0fd5880
Sholahuddin Al Ayubi <sholahuddin95@gmail.com>
2024-02-01 13:59:16 +0700

    Fixed bug

--- web_app/routing_static_asset/main.go
+++ web_app/routing_static_asset/main.go
@@ -7,14 +7,14 @@ import (
 )
 
 func logRequestStatis(r *http.Request) {
-	fmt.Printf("[%s] %s %s (/statis)\n", time.Now().Format("2001-01-01 15:01:01"), r.Method, r.URL.Path)
+	fmt.Printf("[%s] %s %s (/statis)\n", time.Now().Format("2006-01-02 15:04:05"), r.Method, r.URL.Path)
 }
 
 func main() {
 	// http.Handle("/statis/", http.StripPrefix("/statis/", http.FileServer(http.Dir("assets"))))
-	http.HandleFunc("/statis", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/statis/", func(w http.ResponseWriter, r *http.Request) {
 		logRequestStatis(r)
-		http.StripPrefix("/statis", http.FileServer(http.Dir("assets"))).ServeHTTP(w, r)
+		http.StripPrefix("/statis/", http.FileServer(http.Dir("assets"))).ServeHTTP(w, r)
 	})
 	// logRequestStatis(r)
 	fmt.Println("server started at localhost:9000")
